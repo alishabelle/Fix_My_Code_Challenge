d5d647ff6701b37e4fde543819c8b16fc27ecd99
alishabelle <alishabelle98@gmail.com>
2019-10-06 07:15:15 +0000

    push for new checks changed height

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -18,7 +18,7 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def permiterOfMySquare(self):
+    def PermiterOfMySquare(self):
         """ perimeter of square """
         return (self.width * 2) + (self.height * 2)
 
@@ -29,7 +29,7 @@ class Square():
 
 if __name__ == "__main__":
 
-    s = Square(width=12, height=9)
+    s = Square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiterOfMySquare())
+    print(s.PermiterOfMySquare())
