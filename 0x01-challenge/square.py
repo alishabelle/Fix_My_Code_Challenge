77ee70b07780202fd74c9fcee1a76de2b5197ff0
alishabelle <alishabelle98@gmail.com>
2019-10-06 19:32:34 +0000

    started new file

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-""" building class square """
+"""
+creating a new class (Square)
+"""
 
 
 class Square():
-    """ creating area and parameter for square output """
+    """creating the square class area + perimeter"""
 
     width = 0
     height = 0
 
     def __init__(self, width, height):
-        """ defining the square """
+        """instantiate the square"""
         if width == height:
             self.width, self.height = width, height
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """ to get the area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """ perimeter of square """
+        """ to get the perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ printing out the square """
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
-
     s = Square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
